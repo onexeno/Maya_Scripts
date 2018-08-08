dfd04592584293f353dc88529b757810d2c6203b
oneXeno <4219844+onexeno@users.noreply.github.com>
2018-08-08 23:39:32 +0800

    sort_by_second_list

--- PyMel/test.py
+++ PyMel/test.py
@@ -1,10 +1,31 @@
-from pymel import core
+'''
+a = dict()
+
+b = ['x[1]','x[3]','x[6]','y[2]','y[1]','z[2]']
+c = [2,4,1,0,5,9]
+for k in range(0,len(b)):
+    a[c[k]] = b[k]
+
+sorted(a)   
+print a.values()'''
 
 
-def overrideObjectColor(object,objectColor):
-    core.setAttr(object+'.overrideEnabled',1)
-    core.setAttr(object+'.overrideRGBColors',1)
-    core.setAttr(object+'.overrideColorRGB',objectColor)
+
+b = maya.cmds.ls(sl=True,fl=True)
+
+c = [maya.cmds.xform(bComp,q=True,ws=True,t=True)[0] for bComp in b]
+
+
+for i in range(0,len(c)):
+    if c.count(c[i])>1:
+        #print c[i],'\t',c[i]+c.count(c[i])*0.001,'\n'
+        c[i] = c[i]+c.count(c[i])*0.0001
+d = c
+c = sorted(c,reverse=True)
+e = list()
+
+for i in range(0,len(c)):
+    #print d.index(c[i]) 
+    e.append(b[d.index(c[i])])
     
-    
-overrideObjectColor('pCube1',[1,1,1])
+e
